8b6b0675b0e83af519ebf3dd099066020ce47da1
laurawilsone <laura.wils204@gmail.com>
2018-04-12 21:02:55 -0500

    updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,59 +10,76 @@
 
   firebase.initializeApp(config);
   var database = firebase.database();
-  // 2. Button for adding Employees
+  
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
+
     // Grabs user input
-    var Train = $("#train-name-input").val().trim();
-    var Destination = $("#destination-input").val().trim();
-    var First = moment($("#first-input").val().trim(), "HH:mm-military time").format("X");
-    var Frequency = $("#frequency-input").val().trim();
-    // Creates local "temporary" object for holding employee data
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainFirst = moment($("#first-input").val().trim(), "HH:mm-military time").format("X");
+    var trainFrequency = $("#frequency-input").val().trim();
+    
+    // holding data
     var newTrain = {
-      train: Train,
-      destination: Destination,
-      first: First,
-      frequency: Frequency
+      train: trainName,
+      destination: trainDestination,
+      first: trainFirst,
+      frequency: trainFrequency
     };
-    // Uploads employee data to the database
+    
+    // goes into the database
     database.ref().push(newTrain);
-    // Logs everything to console
-    console.log(newTrain.Train);
-    console.log(newTrain.Destination);
-    console.log(newTrain.First);
-    console.log(newTrain.Frequency);
-    // Alert
-    //alert("Employee successfully added");
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.first);
+    console.log(newTrain.frequency);
+   
+   
     // Clears all of the text-boxes
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-input").val("");
     $("#frequency-input").val("");
   });
-  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+
+  // 3. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
     console.log(childSnapshot.val());
+
     // Store everything into a variable.
-    var Train = childSnapshot.val().Train;
-    var Destination = childSnapshot.val().Destination;
-    var First = childSnapshot.val().First;
-    var Frequency = childSnapshot.val().Frequency;
-    // Employee Info
-    console.log(Train);
-    console.log(Destination);
-    console.log(First);
-    console.log(Frequency);
-    // Prettify the employee start
-  //  var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-  //  var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
- //   console.log(empMonths);
-    // Calculate the total billed rate
- //   var empBilled = empMonths * empRate;
- //   console.log(empBilled);
+    var trainName = childSnapshot.val().train;
+    var trainDestination = childSnapshot.val().destination;
+    var trainFirst = childSnapshot.val().first;
+    var trainFrequency = childSnapshot.val().frequency;
+    
+    // train info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainFirst);
+    console.log(trainFrequency);
+
+    // moment
+    var arrivalTime = moment.unix(trainFirst).format("HH:mm");
+ 
+    var trainFrequency = moment().diff(moment.unix(trainFirst), "minutes");
+  // console.log();
+    
+    var remainder = moment().diff(moment.unix(trainFirst), "minutes") % trainFrequency;
+    console.log(remainder);
+    var tminutes = trainFrequency - remainder;
+
+    console.log(arrivalTime)
+    console.log(trainFrequency)
+    console.log(remainder)
+    console.log(tminutes)
+
     // Add each train's data into the table
- //   $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
- //   empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + 
+   trainFirst + "</td><td>" + arrivalTime + "</td><td>" + trainFrequency + "</td><td>");
   });
+
+
